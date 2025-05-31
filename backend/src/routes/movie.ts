afec8ec42cd0f0ed2be5dd1d668e86d844df614d
Abhinav Telukunta <abhi.telukunta@gmail.com>
2025-05-31 14:35:20 -0400

    Feature Request - Multiple Movies

--- backend/src/routes/movie.ts
+++ backend/src/routes/movie.ts
@@ -9,10 +9,12 @@ const postMovie = async (req: Request, res: Response) => {
       res.sendStatus(401);
       return;
     }
-    const { favoriteMovie } = req.body;
-    await prisma.user.update({ where: { email: (req.user as any).email }, data: { favoriteMovie } });
+    const { movies } = req.body;
+    const response = await prisma.user.update({ where: { email: (req.user as any).email }, data: { movies: {set: movies} } });
+    res.send(response);
   } catch (err) {
     console.error('post movie API failed: ', err);
+    res.sendStatus(501);
   }
 };
 
